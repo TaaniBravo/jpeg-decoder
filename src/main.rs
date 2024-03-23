6b244f13e6849fb74349080a5e7e84be8303a7ca
Taani Bravo <70364612+TaaniBravo@users.noreply.github.com>
2024-03-23 12:48:51 -0700

    feat(file): Read file and output hex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = match env::args().next() {
+        Some(path) => path,
+        None => {
+            eprintln!("Problem with parsing the file path");
+            return;
+        }
+    };
+
+    let mut file = match File::open(file_path.as_str()) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Problem opening the file: {:?}", error);
+            return;
+        }
+    };
+
+    let mut buffer = Vec::new();
+    match file.read_to_end(&mut buffer) {
+        Ok(_) => println!("Read {} bytes", buffer.len()),
+        Err(error) => eprintln!("Problem reading the file: {:?}", error),
+    }
+
+    let mut hex_string: String = String::new();
+    for byte in buffer.iter() {
+        hex_string.push_str(format!("{:02X}", byte).as_str());
+    }
+
+    println!("Hex: {}", hex_string);
+    // Do something with the buffer
 }
